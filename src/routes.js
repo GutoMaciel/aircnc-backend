cdbaee8a078948fa23e25d550e3810cc4a6fec54
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-02 14:00:27 -0300

    feature: socket io mobile integration

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ const DashboardController = require('./controllers/DashboardController');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
+
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -17,6 +20,9 @@ routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 routes.get('/dashboard', DashboardController.show);
 
-routes.post('/spots/:spot_id/bookings', BookingController.store )
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
